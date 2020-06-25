a3a5d934e669ee800269176ae97081e9bf1b3c7a
Shayan <Shayansadeghi1996@gmail.com>
2020-06-25 20:28:43 +0430

    added sqlite

--- App.js
+++ App.js
@@ -1,7 +1,51 @@
 import React from "react";
+import * as SQLite from "expo-sqlite";
 
 import Navigator from "./routes/drawer";
 
 export default function App() {
+  const db = SQLite.openDatabase("UserDatabase");
+  db.exec(
+    [
+      {
+        sql:
+          "SELECT name FROM sqlite_master WHERE type='table' AND name='tblUserCards'",
+        args: [],
+      },
+    ],
+    false,
+    (tx, res) => {
+      console.log("Db count:", res[0].rows.length);
+      if (res[0].rows.length === 0) {
+        db.exec(
+          [
+            {
+              sql:
+                "CREATE TABLE tblUserCards(card_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, cardQ VARCHAR(20), cardA VARCHAR(40),cardEx VARCHAR(255) )",
+              args: [],
+            },
+          ],
+          false,
+          (tx, res) => {
+            console.log(res);
+          }
+        );
+      }
+    }
+  );
+
+  db.exec(
+    [
+      {
+        sql: "select * from tblUserCards",
+        args: [],
+      },
+    ],
+    false,
+    (tx, res) => {
+      console.log(res);
+    }
+  );
+
   return <Navigator />;
 }
--- containers/cardModal.js
+++ containers/cardModal.js
@@ -1,23 +1,57 @@
 import React from "react";
-import {
-  Text,
-  View,
-  TouchableOpacity,
-  Modal,
-  TouchableWithoutFeedback,
-  Keyboard,
-  AsyncStorage,
-} from "react-native";
+import { View, Modal, TouchableWithoutFeedback, Keyboard } from "react-native";
+import * as SQLite from "expo-sqlite";
 
 import { GlobalStyles } from "../styles/global";
 
 import CardForm from "../components/cardForm";
 
 export default function CardModal({ visible, setModalOpen }) {
+  const db = SQLite.openDatabase("UserDatabase");
+  db.exec(
+    [
+      {
+        sql:
+          "SELECT name FROM sqlite_master Where type='table' AND name='tblUserCards'",
+        args: [],
+      },
+    ],
+    true,
+    (tx, res) => {
+      console.log(res);
+    }
+  );
   const addCard = values => {
-    console.log(values);
-    console.log("Add");
-    setModalOpen(false);
+    db.exec(
+      [
+        {
+          sql: "INSERT INTO tblUserCards (cardQ,cardA,cardEx) VALUES (?,?,?)",
+          args: [values.question, values.answer, values.example],
+        },
+      ],
+      false,
+      (tx, res) => {
+        console.log("RES: ", res);
+      }
+    );
+    // db.transaction(tx => {
+    //   tx.executeSql(
+    //     "INSERT INTO tblUserCards (question,answer,example) VALUES (?,?,?)",
+    //     [values.question, values.answer, values.example],
+    //     (tx, results) => {
+    //       console.log("RESULTS: ", results.rowsAffected);
+    //       if (results.rowsAffected > 0) {
+    //         console.log("successfully added");
+    //       } else {
+    //         console.log("an error exist");
+    //       }
+    //     }
+    //   );
+    // });
+    console.log(values.question);
+    // console.log("Add");
+
+    // setModalOpen(false);
   };
   return (
     <Modal visible={visible} animationType="slide">
