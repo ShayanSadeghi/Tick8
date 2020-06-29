6e0eda78e96aa2077659efc6d35b222e828bed73
Shayan <Shayansadeghi1996@gmail.com>
2020-06-29 09:56:37 +0430

    created pop up message

---
+++ shared/actionStatus.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default function ActionStatus({ message, messageType }) {
+  const boxColor = messageType + "Box";
+  const textColor = messageType + "Text";
+  return (
+    <View style={styles.boxColor}>
+      <Text style={styles.textColor}>{message}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  successBox: {
+    flexDirection: "row",
+    backgroundColor: "rgba(40, 167, 69, 0.25)",
+    justifyContent: "center",
+    width: "80%",
+    padding: 10,
+    position: "absolute",
+    top: 10,
+    zIndex: 1,
+    borderRadius: 15,
+  },
+  successText: {
+    fontWeight: "bold",
+    color: "#28A745",
+  },
+  warningBox: {
+    flexDirection: "row",
+    backgroundColor: "rgba(255, 193, 7, 0.25)",
+    justifyContent: "center",
+    width: "80%",
+    padding: 10,
+    position: "absolute",
+    top: 10,
+    zIndex: 1,
+    borderRadius: 15,
+  },
+  warningText: {
+    fontWeight: "bold",
+    color: "#FFC107",
+  },
+  dangerBox: {
+    flexDirection: "row",
+    backgroundColor: "rgba(220, 53, 69, 0.25)",
+    justifyContent: "center",
+    width: "80%",
+    padding: 10,
+    position: "absolute",
+    top: 10,
+    zIndex: 1,
+    borderRadius: 15,
+  },
+  dangerText: {
+    fontWeight: "bold",
+    color: "#DC3545",
+  },
+});
